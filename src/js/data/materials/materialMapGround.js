7e957ebc409f9cde47d0e8562150146bdf6c4f41
Rick von Buelow <Rick von Buelow>
2017-01-20 19:43:54 -0800

    Updated scene Details controls

--- src/js/data/materials/materialMapGround.js
+++ src/js/data/materials/materialMapGround.js
@@ -5,8 +5,8 @@ module.exports = {
   ground: {
     maps: [
       { map: "textures/grass/grass_COLOR.jpg" },
-      { specularMap: "textures/grass/grass_SPEC.jpg" },
-      { normalMap: "textures/grass/grass_NRM.jpg" }
+      // { specularMap: "textures/grass/grass_SPEC.jpg" },
+      // { normalMap: "textures/grass/grass_NRM.jpg" }
     ],
     mapProps: { repeatScale: 10, shading: "flat", shininess: 99 },
     // props: {  color: colorPallete.color2.hex }
@@ -14,7 +14,7 @@ module.exports = {
   hedge: {
     maps: [
       { map: "textures/grass/hedge1_COLOR.jpg" },
-      { specularMap: "textures/grass/hedge1_SPEC.jpg" },
+      // { specularMap: "textures/grass/hedge1_SPEC.jpg" },
       // { normalMap: "textures/grass/hedge1_NRM.jpg" },
       { bumpMap: "textures/grass/hedge1_DISP.jpg" }
     ],
